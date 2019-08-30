c1962ae56048254b93068c8dd4ea5bc0969c9855
Da-Colon <dcolonantonio@gmail.com>
2019-08-29 22:33:02 -0400

    added Start function

--- ProjectStudy.py
+++ ProjectStudy.py
@@ -1,3 +1,22 @@
+print('Welcome to FLASHCARDS EXTREME!!!!')
+
+def START():
+    print('What would you like to do? ')
+    usrChoice = input('New Card(A) | Play EXTREME(P) | Exit Game(E): ')
+    if usrChoice == 'a':
+        print('ChangeME')
+    elif usrChoice == 'p':
+        print('EXTREME')
+    elif usrChoice == 'e':
+        print('EXIT')
+    else:
+        print('You did not imnput a correct response')
+        print('Please Try Again')
+        START()
+#Start GAME
+START()
+
+
 class IndexCard:
     def __init__(self,cardFront,cardBack):
         self.cardFront = cardFront
@@ -8,11 +27,6 @@ class IndexCard:
 
 usrInptF = input('Please enter a Term or Phraze ')
 usrInptB = input('Please enter the meaning of ' + usrInptF + ' ')
-
-
-
-
-
 
 card_One = IndexCard(usrInptF,usrInptB) 
 card_One.viewCard()
