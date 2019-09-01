5ad5aa24f389c5512d7f4cc81f0b13f15f9efa78
Da-Colon <dcolonantonio@gmail.com>
2019-09-01 11:38:36 -0400

    Made a few more changes to functionality

--- ProjectStudy.py
+++ ProjectStudy.py
@@ -1,13 +1,33 @@
-import decimal
-import threading
+import sys
 
-print('Welcome to FLASHCARDS EXTREME!!!!')
+from colorama import init
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+from termcolor import cprint 
+from pyfiglet import figlet_format
 
+cprint(figlet_format('Flash Cards ExTREME!', font='starwars'),
+       'yellow', 'on_grey', attrs=['bold'])
+
+
+class IndexCard:
+    def __init__(self, card):
+        self.card = card
+        self.card_front = []
+        self.card_back = []
+    def add_card(self, front, back):
+        self.card_front.append(front)
+        self.card_back.append(back)
+    
+
+card_dict = {}
 def START():
     print('What would you like to do? ')
     usrChoice = input('Add New Card(a) | Play EXTREME(p) | Exit Game(e): ')
     if usrChoice == 'a':
-        
+        #add a loop with? if to increment new card)
+        c = IndexCard('001')
+        c.add_card(input('front: '), input('back: '))
+        START()
     elif usrChoice == 'p':
         print('EXTREME')
     elif usrChoice == 'e':
@@ -21,19 +41,6 @@ def START():
 START()
 
 
-class IndexCard:
-    def __init__(self,cardFront,cardBack):
-        self.cardFront = cardFront
-        self.cardBack = cardBack
-    # def storeCard(self):
-    def viewCard(self):
-         print(self.cardFront)
-         print(self.cardBack)
 
 
-# usrInptF = input('Please enter a Term or Phraze ')                    #Erase
-# usrInptB = input('Please enter the meaning of ' + usrInptF + ' ')     #Erase
-
-card_One = IndexCard(usrInptF,usrInptB) 
-card_One.viewCard()
 
